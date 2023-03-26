874232914a9245c07cec77f59833d1978f475a8f
Tilacin <tilacin45@gmail.com>
2023-03-26 23:24:37 +0300

    Добавили HTML.Разбиваем на компоненты

--- src/App.js
+++ src/App.js
@@ -1,10 +1,38 @@
-import React from 'react';
-import './scss/app.scss'
+import React from "react";
+
+import Header from "./components/Header";
+import Categories from "./components/Categories";
+import Sort from "./components/Sort";
+import PizzaBlock from "./components/PizzaBlock";
+
+import "./scss/app.scss";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello world!</h1>
+      <div className="wrapper">
+        <Header />
+        <div className="content">
+          <div className="container">
+            <div className="content__top">
+              <Categories />
+              <Sort />
+            </div>
+            <h2 className="content__title">Все пиццы</h2>
+            <div className="content__items">
+             <PizzaBlock title="Мексиканская" price="500"/>
+             <PizzaBlock/>
+             <PizzaBlock/>
+             <PizzaBlock/>
+             <PizzaBlock/>
+             <PizzaBlock/>
+             <PizzaBlock/>
+             <PizzaBlock/>
+             <PizzaBlock/>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
