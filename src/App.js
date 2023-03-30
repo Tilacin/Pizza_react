fd3972271375761a80dea5ed255097a2ebd4212d
Tilacin <tilacin45@gmail.com>
2023-03-30 22:14:22 +0300

    Ренденрим все пиццы. Выбираем характеристики пиццы через useState. Создаем PizzaBlock через "map".

--- src/App.js
+++ src/App.js
@@ -3,9 +3,12 @@ import React from "react";
 import Header from "./components/Header";
 import Categories from "./components/Categories";
 import Sort from "./components/Sort";
-import PizzaBlock from "./components/PizzaBlock";
+
+
+import pizzas from "./assets/pizza.json";
 
 import "./scss/app.scss";
+import PizzaBlock from "./components/PizzaBlock";
 
 function App() {
   return (
@@ -20,15 +23,9 @@ function App() {
             </div>
             <h2 className="content__title">Все пиццы</h2>
             <div className="content__items">
-             <PizzaBlock title="Мексиканская" price="500"/>
-             <PizzaBlock title="Мексиканская" price="500"/>
-             <PizzaBlock title="Мексиканская" price="500"/>
-             <PizzaBlock title="Мексиканская" price="500"/>
-             <PizzaBlock title="Мексиканская" price="500"/>
-             <PizzaBlock title="Мексиканская" price="500"/>
-             <PizzaBlock title="Мексиканская" price="500"/>
-             <PizzaBlock title="Мексиканская" price="500"/>
-             <PizzaBlock title="Мексиканская" price="500"/>
+              {pizzas.map((obj) => (
+                <PizzaBlock {...obj} />
+              ))}
             </div>
           </div>
         </div>
