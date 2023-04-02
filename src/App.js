4ca5a4e8ea20660985e3b55aaba43500cc5f613b
Tilacin <tilacin45@gmail.com>
2023-04-02 21:40:40 +0300

    Перенесли пиццы на сервер

--- src/App.js
+++ src/App.js
@@ -4,13 +4,19 @@ import Header from "./components/Header";
 import Categories from "./components/Categories";
 import Sort from "./components/Sort";
 
-import pizzas from "./assets/pizza.json";
+//import pizzas from "./assets/pizza.json";
 
 import "./scss/app.scss";
 import PizzaBlock from "./components/PizzaBlock";
 
 function App() {
-
+  const [items, setItems] = React.useState([]);
+  //получаем и отрисовываем пиццы с бэка, что бы бесконечно не отрисовывалось оборачиваем в .useEffect()
+  React.useEffect(() => {
+    fetch("https://6429bc455a40b82da4d97645.mockapi.io/items")
+      .then((res) => res.json())
+      .then((arr) => setItems(arr));
+  }, []);
 
   return (
     <div className="App">
@@ -24,7 +30,7 @@ function App() {
             </div>
             <h2 className="content__title">Все пиццы</h2>
             <div className="content__items">
-              {pizzas.map((obj) => (
+              {items.map((obj) => (
                 <PizzaBlock key={obj.id} {...obj} />
               ))}
             </div>
