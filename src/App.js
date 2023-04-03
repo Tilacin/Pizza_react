1dbe87d8f07d1abd88078f09e98b8f9272f0e40a
Tilacin <tilacin45@gmail.com>
2023-04-04 00:08:35 +0300

    Сделали скелетлн

--- src/App.js
+++ src/App.js
@@ -7,10 +7,13 @@ import Sort from "./components/Sort";
 //import pizzas from "./assets/pizza.json";
 
 import "./scss/app.scss";
-import PizzaBlock from "./components/PizzaBlock";
+import PizzaBlock from "./components/PizzaBlock/PizzaBlock";
+import { Skeleton } from "./components/PizzaBlock/Skeleton";
 
 function App() {
   const [items, setItems] = React.useState([]);
+  const [isLoading, setIsLoading] = React.useState(true); //скелетон или пиццы
+  
   //получаем и отрисовываем пиццы с бэка, что бы бесконечно не отрисовывалось оборачиваем в .useEffect()
   React.useEffect(() => {
     fetch("https://6429bc455a40b82da4d97645.mockapi.io/items")
@@ -30,9 +33,9 @@ function App() {
             </div>
             <h2 className="content__title">Все пиццы</h2>
             <div className="content__items">
-              {items.map((obj) => (
-                <PizzaBlock key={obj.id} {...obj} />
-              ))}
+              {isLoading
+                ? [...new Array(6)].map((_, index) => <Skeleton key={index}/>) /*{пока идёт загрузка - отображаем скелетон}*/
+                : items.map((obj) => <PizzaBlock key={obj.id} {...obj} />)} 
             </div>
           </div>
         </div>
