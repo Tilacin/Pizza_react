4fd3108b419ea51cc33738635fc154192746f515
Tilacin <tilacin45@gmail.com>
2023-04-18 21:04:24 +0300

    Добавили редакс тулкит

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { decrement, increment } from "./redux/slices/filterSlice";
 
 import Header from "./components/Header";
 import Home from "./pages/Home";
@@ -8,21 +10,38 @@ import { Route, Routes } from "react-router-dom";
 
 import "./scss/app.scss";
 
-export const SearchContext = React.createContext();//контекст
+export const SearchContext = React.createContext(); //контекст
 
 function App() {
   const [searchValue, setSearchValue] = React.useState(""); //инпут
+  const count = useSelector((state) => state.counter.count);
+  const dispatch = useDispatch();
 
   return (
     <div className="wrapper">
-      <SearchContext.Provider value={{searchValue, setSearchValue}}>{/* Обернули в контекст и 
+      <button
+        aria-label="Increment value"
+        onClick={() => dispatch(increment())}
+      >
+        Increment
+      </button>
+      <span>{count}</span>
+      <button
+        aria-label="Decrement value"
+        onClick={() => dispatch(decrement())}
+      >
+        Decrement
+      </button>
+
+      <SearchContext.Provider value={{ searchValue, setSearchValue }}>
+        {/* Обернули в контекст и 
       теперь параметры можно пробросить сразу в нужный компонент, а не по цепочке вложенности */}
-        <Header  />
+        <Header />
         <div className="container">
           {/* Делаем роутинг */}
           <Routes>
             {/* По гл. пути рендерим <Home/> */}
-            <Route path="/" element={<Home  />} />{" "}
+            <Route path="/" element={<Home />} />{" "}
             <Route path="/cart" element={<Cart />} />{" "}
             {/* если по пути ни чего не подошло, то рендерим  <NotFound /> */}
             <Route path="*" element={<NotFound />} />{" "}
