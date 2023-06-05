4afabd6b173555352afda762be41dfb690b71872
Tilacin <tilacin45@gmail.com>
2023-06-05 20:44:50 +0300

    Начали переписывать на TS

--- src/pages/FullPizza.tsx
+++ src/pages/FullPizza.tsx
@@ -1,15 +1,20 @@
 import React from 'react'
 import axios from 'axios'
 import { useParams, useNavigate } from 'react-router-dom'
-const FullPizza = () => {
-    const [pizza, setPizza] = React.useState()
+
+const FullPizza: React.FC = () => {
+    const [pizza, setPizza] = React.useState<{
+        imageUrl: string
+        title: string
+        price: number
+    }>()
     const { id } = useParams()
     const navigate = useNavigate()
 
     React.useEffect(() => {
         async function fetchPizza() {
             try {
-                const { data } =await axios.get('https://6429bc455a40b82da4d97645.mockapi.io/items/'+id)
+                const { data } = await axios.get('https://6429bc455a40b82da4d97645.mockapi.io/items/' + id)
                 setPizza(data)
             } catch (error) {
                 alert('Ошибка при получении пиццы!')
@@ -17,10 +22,10 @@ const FullPizza = () => {
             }
         }
         fetchPizza()
-    }, [])
+    },[])
 
     if (!pizza) {
-        return 'Загрузка...'
+        return <>Загрузка...</>
     }
     return (
         <div className='container'>
